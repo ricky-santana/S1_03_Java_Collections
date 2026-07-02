eae9ad914ae9ba85a9f579bd112f559436c6122a
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-07-02 15:25:41 +0200

    Nivel 1 ejercicio 3: COMPLETADO

--- src/nivel_1/ejercicio_3/Game.java
+++ src/nivel_1/ejercicio_3/Game.java
@@ -1,5 +1,6 @@
 package nivel_1.ejercicio_3;
 
+import javax.xml.transform.Source;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -12,17 +13,33 @@ public class Game {
         System.out.println("Introduce tu nombre");
         String userName = scanner.nextLine();
 
-        byte puntos = 0;
+        byte points = 0;
         System.out.println("Juguemos " + userName + " tienes 10 rondas para lograr la máxima puntuación" );
 
         byte rondas = 0;
         while (rondas < 11){
-            byte random = (byte) (Math.random() * 51 + 1);
+            byte random = (byte) (Math.random() * 51);
             String randomCountry = new TextToArray().getCountries().get(random);
-            System.out.println("Ronda nº" + rondas +1 );
-            System.out.println("País " + randomCountry); //
+
+            System.out.println("Ronda nº" + (rondas +1) );
+            System.out.println("Cual es la capital de: " + randomCountry);
+            String answer = scanner.nextLine();
+
+            String respuesta;
+            if (countryCapitals.get(randomCountry).equals(answer)){
+                points++;
+                respuesta = ("Bingo, has ganado 1 punto");
+            } else {
+                respuesta = ("Error, Has fallado");
+            }
+            rondas++;
+            System.out.println(respuesta +
+                    "\nTotal de puntos: " + points +
+                    "\nRondas restantes: " + (11 - rondas) );
+
 
         }
+        System.out.println("Se acabó " + userName + " ya has jugado suficiente, a programar!!!");
 
     }
 }
