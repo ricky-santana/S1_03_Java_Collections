dd2011c10251e81bae34bf51ae743665cbd7e07d
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-07-02 13:16:52 +0200

    Nivel 1 ejercicio 2: creada otra lista para poner rellenarla con ListIterator.HasPrevious

--- src/nivel_1/ejercicio_2/Main.java
+++ src/nivel_1/ejercicio_2/Main.java
@@ -1,7 +1,9 @@
 package nivel_1.ejercicio_2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     void main() {
@@ -20,10 +22,25 @@ public class Main {
             inverseList.addFirst(n);
         }
 
-        System.out.println("Lista inversa: ");
+        System.out.println("Lista inversa con un bucle normal: ");
         for (int i = 0; i < intList.size(); i++){
             System.out.println(inverseList.get(i) + "\n");
         }
+
+
+        System.out.println("Lista inversa con un ListIterator y HasPrevious: ");
+        ListIterator <Integer> intListIterator = intList.listIterator();
+        List <Integer> inverseList2 = new ArrayList<Integer>();
+
+        while (intListIterator.hasPrevious()){
+            inverseList2.add(intListIterator.previous());
+        }
+        for (int i = 0; i < intList.size(); i++){
+            System.out.println(inverseList.get(i) + "\n");
+        }
+
+
+
 
 
     }
