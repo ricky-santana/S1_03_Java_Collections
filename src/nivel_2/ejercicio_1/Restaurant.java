69262bcb35c1c4f097795684394f3c633459c5e1
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-07-05 20:59:30 +0200

    S1_03 Nivel 2 Ejercicio 1: creada la clase como pide el enunciado, equals y hashCode incluidos

---
+++ src/nivel_2/ejercicio_1/Restaurant.java
@@ -0,0 +1,25 @@
+package nivel_2.ejercicio_1;
+
+import java.util.Objects;
+
+public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return score == that.score && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+}
